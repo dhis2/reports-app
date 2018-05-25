872d5b529f1fd5593ff5050ddb15479c6d945bfb
Joao <joao.antunes@present-technologies.com>
2018-05-25 08:55:54 +0100

    D2 integration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
+/* React */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+/* d2-ui */
+import HeaderBar from '@dhis2/d2-ui-header-bar';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      return (<HeaderBar d2={this.props.d2} />);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,25 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// D2
-import { getManifest, getUserSettings } from 'd2/lib/d2';
-// import D2UIApp from 'd2-ui/lib/app/D2UIApp';
+/* d2 */
+import { getManifest, init } from 'd2/lib/d2';
 
 import './index.css';
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-// init d2
+/* init d2 */
 getManifest('manifest.webapp').then((manifest) => {
-    /*
     const api = process.env.REACT_APP_DHIS2_API_VERSION ? `/${process.env.REACT_APP_DHIS2_API_VERSION}` : '/';
     const baseUrl =
         process.env.NODE_ENV === 'production'
             ? `${manifest.getBaseUrl()}/api/${manifest.dhis2.apiVersion}`
             : `${process.env.REACT_APP_DHIS2_BASE_URL}/api${api}`;
-    */
-    ReactDOM.render(<App />, document.getElementById('root'));
-}).then(getUserSettings);
+
+    // init d2 with configs
+    init({
+        baseUrl,
+        schemas: [
+            'organisationUnit',
+            'dataSet',
+        ],
+    }).then(d2 => {
+        ReactDOM.render(<App d2={d2}/>, document.getElementById('app'));
+    });
+});
 
 registerServiceWorker();
