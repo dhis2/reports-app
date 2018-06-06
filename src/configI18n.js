90c6ae7a09c1f001b830d410e0d2d8e212f83d7c
Joao <joao.antunes@present-technologies.com>
2018-06-06 14:48:16 +0100

    Storing translations for d2-ui components

--- src/configI18n.js
+++ src/configI18n.js
@@ -1,4 +1,5 @@
 import i18n from './locales';
+import { i18nKeys } from './i18n';
 
 const isLangRTL = (code) => {
     const langs = ['ar', 'fa', 'ur'];
@@ -6,13 +7,25 @@ const isLangRTL = (code) => {
     return langs.includes(code) || prefixed.filter(c => code.startsWith(c)).length > 0;
 };
 
-const configI18n = (userSettings) => {
+export const configI18n = (userSettings) => {
     const lang = userSettings.keyUiLocale;
     if (isLangRTL(lang)) {
         document.body.setAttribute('dir', 'rtl');
     }
 
     i18n.changeLanguage(lang);
+};
+
+export const injectTranslationsToD2 = (d2) => {
+    if (d2) {
+        const translations = {};
+        const translationKeys = Object.keys(i18nKeys.d2UiComponents);
+        for (let i = 0; i < translationKeys.length; i++) {
+            const key = translationKeys[i];
+            translations[key] = i18n.t(i18nKeys.d2UiComponents[key]);
+        }
+        Object.assign(d2.i18n.translations, translations);
+    }
 };
 
 export default configI18n;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { HashRouter, withRouter } from 'react-router-dom';
 import { init, getManifest, getUserSettings } from 'd2/lib/d2';
 
 /* i18n */
-import configI18n from './configI18n';
+import { configI18n, injectTranslationsToD2 } from './configI18n';
 
 import './index.css';
 import App from './App';
@@ -40,6 +40,7 @@ getManifest('manifest.webapp').then((manifest) => {
         .then(getUserSettings)
         .then(configI18n)
         .then(() => {
+            injectTranslationsToD2(d2Instance);
             ReactDOM.render(
                 <HashRouter>
                     <AppComponent
