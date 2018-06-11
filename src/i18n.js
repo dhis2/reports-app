76320ff16f9c79f062df3443420e85aa725a6c8a
Joao <joao.antunes@present-technologies.com>
2018-06-11 09:22:00 +0100

    Data Sets Dropdown

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,7 @@
 export const i18nKeys = {
+    datasetsDropdown: {
+        hintText: 'Select Data Set',
+    },
     standardReport: {
         homeLabel: 'Standard Report',
         homeAction: 'View Reports',
--- src/pages/data-set-report/DataSetReport.js
+++ src/pages/data-set-report/DataSetReport.js
@@ -4,6 +4,7 @@ import React from 'react';
 /* App components */
 import Page from '../Page';
 import PageHelper from '../../components/page-helper/PageHelper';
+import Datasets from '../../components/datasets-dropdown/DatasetsDropdown';
 
 /* utils */
 import { getDocsUrl } from '../../helpers/docs';
@@ -11,12 +12,15 @@ import { getDocsUrl } from '../../helpers/docs';
 class DataSetReport extends Page {
     render() {
         return (
-            <h1>
-                Data Set Report
-                <PageHelper
-                    url={getDocsUrl(this.props.d2.system.version, this.props.sectionKey)}
-                />
-            </h1>
+            <div>
+                <h1>
+                    Data Set Report
+                    <PageHelper
+                        url={getDocsUrl(this.props.d2.system.version, this.props.sectionKey)}
+                    />
+                </h1>
+                <Datasets />
+            </div>
         );
     }
 }
