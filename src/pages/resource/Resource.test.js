6c043cab175720cc081fa44bb8389149455ed405
MDHomem <mdhomem@gmail.com>
2018-08-17 15:05:48 +0100

    Resource pagination functions tests

--- src/pages/resource/Resource.test.js
+++ src/pages/resource/Resource.test.js
@@ -260,5 +260,62 @@ describe('Test <Resource /> actions:', () => {
         expect(spy).toHaveBeenCalled();
     });
 
+    /* Pagination */
+    describe('Test pagination actions:', () => {
+        let wrapper;
+        beforeEach(() => {
+            wrapper = ownShallow();
+        });
+
+        it('Should have a "hasNextPage" function.', () => {
+            expect(wrapper.instance().hasNextPage).toBeDefined();
+            expect(typeof wrapper.instance().hasNextPage).toBe('function');
+            wrapper.setState({
+                pager: { page: 1, pageCount: 10 },
+            });
+            expect(wrapper.instance().hasNextPage()).toBe(true);
+            wrapper.setState({
+                pager: { page: 10, pageCount: 10 },
+            });
+            expect(wrapper.instance().hasNextPage()).toBe(false);
+        });
+
+        it('Should have a "hasPreviousPage" function.', () => {
+            expect(wrapper.instance().hasPreviousPage).toBeDefined();
+            expect(typeof wrapper.instance().hasPreviousPage).toBe('function');
+            wrapper.setState({
+                pager: { page: 10, pageCount: 10 },
+            });
+            expect(wrapper.instance().hasPreviousPage()).toBe(true);
+            wrapper.setState({
+                pager: { page: 0, pageCount: 10 },
+            });
+            expect(wrapper.instance().hasPreviousPage()).toBe(false);
+        });
+
+        it('Should have a "onNextPageClick" function.', () => {
+            const spy = spyOn(Resource.prototype, 'loadDocuments');
+            expect(wrapper.instance().onNextPageClick).toBeDefined();
+            expect(typeof wrapper.instance().onNextPageClick).toBe('function');
+            wrapper.setState({
+                pager: { page: 1, pageCount: 10 },
+            });
+            wrapper.instance().onNextPageClick();
+            expect(spy).toHaveBeenCalledWith({ page: 2, pageCount: 10 }, "");
+        });
+
+        it('Should have a "onPreviousPageClick" function.', () => {
+            const spy = spyOn(Resource.prototype, 'loadDocuments');
+            expect(wrapper.instance().onPreviousPageClick).toBeDefined();
+            expect(typeof wrapper.instance().onPreviousPageClick).toBe('function');
+            wrapper.setState({
+                pager: { page: 2, pageCount: 10 },
+            });
+            wrapper.instance().onPreviousPageClick();
+            expect(spy).toHaveBeenCalledWith({ page: 1, pageCount: 10 }, "");
+        });
+
+    });
+
 });
 
