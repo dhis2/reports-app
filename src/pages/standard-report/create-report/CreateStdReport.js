f34b370761c01ad34c9259184ae8588b8c6792eb
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-02-04 16:01:31 +0100

    refactor: use setState instead of setting state directly

--- src/pages/standard-report/create-report/CreateStdReport.js
+++ src/pages/standard-report/create-report/CreateStdReport.js
@@ -1,26 +1,29 @@
 /* React */
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
 /* material-ui */
-import { Dialog } from 'material-ui';
+import { Dialog } from 'material-ui'
 
 /* d2-ui */
-import { Button, SelectField } from '@dhis2/d2-ui-core';
+import { Button, SelectField } from '@dhis2/d2-ui-core'
 
 /* app components */
-import
-OrganisationUnitsTree
-    from '../../../components/available-organisation-units-tree/AvailableOrganisationUnitsTree';
+import OrganisationUnitsTree from '../../../components/available-organisation-units-tree/AvailableOrganisationUnitsTree'
 
 /* i18n */
-import { i18nKeys } from '../../../utils/i18n/i18nKeys';
-import i18n from '../../../utils/i18n/locales';
+import { i18nKeys } from '../../../utils/i18n/i18nKeys'
+import i18n from '../../../utils/i18n/locales'
 
 /* styles */
-import appStyles from '../../../utils/styles';
+import appStyles from '../../../utils/styles'
 
-import { REPORTS_ENDPOINT, REPORT_MODE, GET_REPORT_AS_ENDPOINT, TYPES } from '../standard.report.conf';
+import {
+    REPORTS_ENDPOINT,
+    REPORT_MODE,
+    GET_REPORT_AS_ENDPOINT,
+    TYPES,
+} from '../standard.report.conf'
 
 class CreateStdReport extends PureComponent {
     static propTypes = {
@@ -30,157 +33,191 @@ class CreateStdReport extends PureComponent {
         onGetHtmlReport: PropTypes.func,
         open: PropTypes.bool.isRequired,
         onError: PropTypes.func.isRequired,
-    };
+    }
 
     static defaultProps = {
         onGetHtmlReport: () => {},
-    };
+    }
 
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             selectedOrgUnitId: null,
             selectedPeriod: null,
-        };
+        }
     }
 
     componentDidMount() {
         // TODO: validate if always report
-        this.loadReportParams(REPORT_MODE.REPORT);
+        this.loadReportParams(REPORT_MODE.REPORT)
     }
 
     componentWillReceiveProps(nextProps) {
         if (this.props.selectedReport.id !== nextProps.selectedReport.id) {
-            this.loadReportParams(REPORT_MODE.REPORT);
+            this.loadReportParams(REPORT_MODE.REPORT)
         } else {
-            this.getReport();
+            this.getReport()
         }
     }
 
-    onChangePeriod = (selectedPeriod) => {
-        this.setState({ selectedPeriod });
-    };
+    onChangePeriod = selectedPeriod => {
+        this.setState({ selectedPeriod })
+    }
 
-    onChangeOrgUnit = (selectedOrgUnitId) => {
-        this.setState({ selectedOrgUnitId });
-    };
+    onChangeOrgUnit = selectedOrgUnitId => {
+        this.setState({ selectedOrgUnitId })
+    }
 
     // TODO: Possible merge this and getExcelReport method
     getReport = () => {
-        const api = this.props.d2.Api.getApi();
-        const typeEndpoint = this.props.selectedReport.type === TYPES.HTML ?
-            GET_REPORT_AS_ENDPOINT.HTML : GET_REPORT_AS_ENDPOINT.PDF;
-        let url = `${REPORTS_ENDPOINT}/${this.props.selectedReport.id}/${typeEndpoint}?t=${new Date().getTime()}`;
+        const api = this.props.d2.Api.getApi()
+        const typeEndpoint =
+            this.props.selectedReport.type === TYPES.HTML
+                ? GET_REPORT_AS_ENDPOINT.HTML
+                : GET_REPORT_AS_ENDPOINT.PDF
+        let url = `${REPORTS_ENDPOINT}/${
+            this.props.selectedReport.id
+        }/${typeEndpoint}?t=${new Date().getTime()}`
         if (this.state.selectedOrgUnitId) {
-            url = `${url}&ou=${this.state.selectedOrgUnitId}`;
+            url = `${url}&ou=${this.state.selectedOrgUnitId}`
         }
         if (this.state.selectedPeriod) {
-            url = `${url}&pe=${this.state.selectedPeriod.id}`;
+            url = `${url}&pe=${this.state.selectedPeriod.id}`
         }
         if (this.props.selectedReport.type === TYPES.HTML) {
-            api.get(url).then((response) => {
-                this.props.onGetHtmlReport(response);
-            }).catch((error) => {
-                this.props.onError(error);
-            });
+            api.get(url)
+                .then(response => {
+                    this.props.onGetHtmlReport(response)
+                })
+                .catch(error => {
+                    this.props.onError(error)
+                })
         } else {
-            window.open(`${api.baseUrl}/${url}`);
+            window.open(`${api.baseUrl}/${url}`)
         }
-    };
+    }
 
     getExcelReport = () => {
-        const api = this.props.d2.Api.getApi();
-        const typeEndpoint = GET_REPORT_AS_ENDPOINT.XLS;
-        let url = `${REPORTS_ENDPOINT}/${this.props.selectedReport.id}/${typeEndpoint}?t=${new Date().getTime()}`;
+        const api = this.props.d2.Api.getApi()
+        const typeEndpoint = GET_REPORT_AS_ENDPOINT.XLS
+        let url = `${REPORTS_ENDPOINT}/${
+            this.props.selectedReport.id
+        }/${typeEndpoint}?t=${new Date().getTime()}`
         if (this.state.selectedOrgUnitId) {
-            url = `${url}&ou=${this.state.selectedOrgUnitId}`;
+            url = `${url}&ou=${this.state.selectedOrgUnitId}`
         }
         if (this.state.selectedPeriod) {
-            url = `${url}&pe=${this.state.selectedPeriod.id}`;
+            url = `${url}&pe=${this.state.selectedPeriod.id}`
         }
         if (this.props.selectedReport.type === TYPES.HTML) {
-            api.get(url).then((response) => {
-                this.props.onGetHtmlReport(response);
-            }).catch((error) => {
-                this.props.onError(error);
-            });
+            api.get(url)
+                .then(response => {
+                    this.props.onGetHtmlReport(response)
+                })
+                .catch(error => {
+                    this.props.onError(error)
+                })
         } else {
-            window.open(`${api.baseUrl}/${url}`);
+            window.open(`${api.baseUrl}/${url}`)
         }
-    };
+    }
 
-    loadReportParams = (reportMode) => {
-        const api = this.props.d2.Api.getApi();
-        const url = `${REPORTS_ENDPOINT}/${this.props.selectedReport.id}/parameters?mode=${reportMode}`;
+    loadReportParams = reportMode => {
+        const api = this.props.d2.Api.getApi()
+        const url = `${REPORTS_ENDPOINT}/${
+            this.props.selectedReport.id
+        }/parameters?mode=${reportMode}`
         if (api) {
-            api.get(url).then((response) => {
-                if (response && this.isSet(response)) {
-                    if (response.periods) {
-                        this.setState({ selectedPeriod: response.periods[0] });
+            api.get(url)
+                .then(response => {
+                    if (response && this.isSet(response)) {
+                        if (response.periods) {
+                            this.setState({
+                                selectedPeriod: response.periods[0],
+                            })
+                        }
+                        this.setState({ ...response }) // params && || periods
+                    } else {
+                        this.getReport()
                     }
-                    this.setState({ ...response }); // params && || periods
-                } else {
-                    this.getReport();
-                }
-            }).catch((error) => {
-                this.props.onError(error);
-            });
+                })
+                .catch(error => {
+                    this.props.onError(error)
+                })
         }
-    };
+    }
 
-    isSet = (obj = this.state) => !!(obj.params &&
-        (obj.params.paramReportingPeriod || this.isOrganisationUnitSet(obj)));
+    isSet = (obj = this.state) =>
+        !!(
+            obj.params &&
+            (obj.params.paramReportingPeriod || this.isOrganisationUnitSet(obj))
+        )
 
-    isOrganisationUnitSet = (obj = this.state) => !!obj.params && (obj.params.paramOrganisationUnit ||
-        obj.params.paramParentOrganisationUnit ||
-        obj.params.paramGrandParentOrganisationUnit);
+    isOrganisationUnitSet = (obj = this.state) =>
+        !!obj.params &&
+        (obj.params.paramOrganisationUnit ||
+            obj.params.paramParentOrganisationUnit ||
+            obj.params.paramGrandParentOrganisationUnit)
 
     isReportingPeriod = () =>
-        !!this.state.periods && this.state.periods.length > 0 &&
-        this.state.params && this.state.params.paramReportingPeriod;
+        !!this.state.periods &&
+        this.state.periods.length > 0 &&
+        this.state.params &&
+        this.state.params.paramReportingPeriod
 
     displayPeriods = () => {
         if (this.isReportingPeriod()) {
             if (!this.state.selectedPeriod) {
-                this.state.selectedPeriod = this.state.periods[0];
+                this.setState({
+                    selectedPeriod: this.state.periods[0],
+                })
             }
             return (
                 <SelectField
                     selector={'periods'}
                     label={i18n.t(i18nKeys.standardReport.reportingPeriod)}
                     items={this.state.periods}
-                    value={this.state.selectedPeriod ? this.state.selectedPeriod.id : this.state.periods[0].id}
+                    value={
+                        this.state.selectedPeriod
+                            ? this.state.selectedPeriod.id
+                            : this.state.periods[0].id
+                    }
                     onChange={this.onChangePeriod}
                 />
-            );
+            )
         }
-        return null;
-    };
+        return null
+    }
 
     displayOrgUnitTree = () => {
         if (this.state.params && this.isOrganisationUnitSet()) {
             return (
                 <div style={{ height: 350 }}>
                     <div>
-                        {i18n.t(i18nKeys.availableOrganisationUnitsTree.treeLabel)}
+                        {i18n.t(
+                            i18nKeys.availableOrganisationUnitsTree.treeLabel
+                        )}
                     </div>
-                    <OrganisationUnitsTree d2={this.props.d2} onChange={this.onChangeOrgUnit} />
+                    <OrganisationUnitsTree
+                        d2={this.props.d2}
+                        onChange={this.onChangeOrgUnit}
+                    />
                 </div>
-            );
+            )
         }
-        return null;
-    };
+        return null
+    }
 
     validate = () => {
         if (this.isReportingPeriod() && this.isOrganisationUnitSet()) {
-            return !!(this.state.selectedPeriod && this.state.selectedOrgUnitId);
+            return !!(this.state.selectedPeriod && this.state.selectedOrgUnitId)
         } else if (this.isReportingPeriod()) {
-            return !!this.state.selectedPeriod;
+            return !!this.state.selectedPeriod
         } else if (this.isOrganisationUnitSet()) {
-            return !!this.state.selectedOrgUnitId;
+            return !!this.state.selectedOrgUnitId
         }
-        return false;
-    };
+        return false
+    }
 
     render() {
         const actions = [
@@ -215,7 +252,7 @@ class CreateStdReport extends PureComponent {
                     {i18n.t(i18nKeys.buttons.getReport)}
                 </Button>
             </span>,
-        ];
+        ]
 
         if (this.isSet()) {
             return (
@@ -232,10 +269,10 @@ class CreateStdReport extends PureComponent {
                         {this.displayOrgUnitTree()}
                     </div>
                 </Dialog>
-            );
+            )
         }
-        return (null);
+        return null
     }
 }
 
-export default CreateStdReport;
+export default CreateStdReport
