63d1e76de595984dea8d0fb41d5b81be282b88a6
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-02-19 10:47:41 +0100

    refactor: use mock store for testing loading data set dimensions

--- src/redux/actions/dataSetDimensions.js
+++ src/redux/actions/dataSetDimensions.js
@@ -1,7 +1,7 @@
 import humanReadableErrorMessage from '../../utils/humanReadableErrorMessage'
 import i18n from '../../utils/i18n/locales'
-import { loadDimensions } from './dataSetDimensions/asyncThunks'
 import { showWarningSnackBar } from './feedback'
+import { getDimensions } from '../../utils/api'
 
 export const actionTypes = {
     LOADING_DIMENSIONS_START: 'LOADING_DIMENSIONS_START',
@@ -10,34 +10,62 @@ export const actionTypes = {
     SELECT_DIMENSION_OPTION: 'SELECT_DIMENSION_OPTION',
 }
 
+/**
+ * @returns {Object}
+ */
 export const loadingDimensionsStart = () => ({
     type: actionTypes.LOADING_DIMENSIONS_START,
 })
 
+/**
+ * @param {Array} dimensions
+ * @returns {Object}
+ */
 export const loadingDimensionsSuccess = dimensions => ({
     type: actionTypes.LOADING_DIMENSIONS_SUCCESS,
     payload: dimensions,
+})
+
+export const loadingDimensionsError = () => ({
+    type: actionTypes.LOADING_DIMENSIONS_ERROR,
 })
 
 /**
  * @param (Error) error
  * @returns {Function}
  */
-export const loadingDimensionsError = error => dispatch => {
+export const loadingDimensionsErrorWithFeedback = error => dispatch => {
     const defaultMessage = i18n.t(
         'An error occurred while loading the data set dimensions'
     )
     const displayMessage = humanReadableErrorMessage(error, defaultMessage)
     dispatch(showWarningSnackBar(displayMessage))
-    dispatch({
-        type: actionTypes.LOADING_DIMENSIONS_ERROR,
-        payload: displayMessage,
-    })
+    dispatch(loadingDimensionsError())
 }
 
+/**
+ * @param {string} dimension
+ * @param {string} value
+ * @returns {Object}
+ */
 export const selectDimensionOption = (dimension, value) => ({
     type: actionTypes.SELECT_DIMENSION_OPTION,
     payload: { dimension, value },
 })
 
-export { loadDimensions }
+/**
+ * @returns {Function}
+ */
+export const loadDimensions = () => (dispatch, getState) => {
+    dispatch(loadingDimensionsStart())
+
+    const { dataSet } = getState()
+    return getDimensions(dataSet.selected.id)
+        .then(response =>
+            response.error
+                ? Promise.reject(response.error)
+                : Promise.resolve(response.dimensions)
+        )
+        .then(dimensions => dispatch(loadingDimensionsSuccess(dimensions)))
+        .catch(error => dispatch(loadingDimensionsErrorWithFeedback(error)))
+}
