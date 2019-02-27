4659dd548bd55f4f47d402db8a63bb47a7efcb7e
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-02-27 14:48:23 +0100

    refactor(group sets dropdown): connect with redux state

--- src/components/GroupSetsDropdown.js
+++ src/components/GroupSetsDropdown.js
@@ -1,91 +1,54 @@
-/* React */
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-
-/* d2-ui components */
 import { DropDown } from '@dhis2/d2-ui-core'
-
-/* App context */
-import AppContext from '../pages/AppContext'
-
-/* i18n */
+import { connect } from 'react-redux'
 import i18n from '../utils/i18n/locales'
-import { i18nKeys } from '../utils/i18n/i18nKeys'
-
-/* styles */
 import styles from '../utils/styles'
+import { setGroupSet } from '../redux/actions/orgUnitGroupSets'
+
+const label = i18n.t('Group Set')
+const hintText = i18n.t('Select Group Set')
 
 export class GroupSetsDropdown extends PureComponent {
     static propTypes = {
-        d2: PropTypes.object.isRequired,
-        onChange: PropTypes.func,
-        label: PropTypes.string,
+        collection: PropTypes.array.isRequired,
+        selected: PropTypes.string.isRequired,
+        selectGroupSet: PropTypes.func.isRequired,
         fullWidth: PropTypes.bool,
     }
 
     static defaultProps = {
-        onChange: () => {},
-        label: i18n.t(i18nKeys.groupSetsDropdown.label),
         fullWidth: true,
-    }
-
-    constructor() {
-        super()
-
-        this.state = {
-            groupSets: [],
-            selected: null,
-        }
-    }
-
-    componentDidMount() {
-        const d2 = this.props.d2
-        d2.models.organisationUnitGroupSet
-            .list({
-                paging: false,
-                fields: 'id,displayName',
-            })
-            .then(groupSetsResponse => {
-                this.setState({
-                    groupSets: groupSetsResponse.toArray(),
-                })
-            })
-            .catch(() => {
-                // TODO Manage error
-            })
-    }
-
-    onChange = event => {
-        const value = event.target.value
-        this.setState({
-            selected: value,
-        })
-
-        if (this.props.onChange) {
-            this.props.onChange(value)
-        }
     }
 
     render() {
         return (
             <div>
-                <span style={styles.formLabel}>{this.props.label}</span>
+                <span style={styles.formLabel}>{label}</span>
                 <DropDown
                     fullWidth={this.props.fullWidth}
-                    value={this.state.selected}
-                    onChange={this.onChange}
-                    menuItems={this.state.groupSets}
+                    value={this.props.selected}
+                    onChange={this.props.selectGroupSet}
+                    menuItems={this.props.collection}
                     includeEmpty
-                    emptyLabel={i18n.t(i18nKeys.groupSetsDropdown.hintText)}
-                    hintText={i18n.t(i18nKeys.groupSetsDropdown.hintText)}
+                    emptyLabel={hintText}
+                    hintText={hintText}
                 />
             </div>
         )
     }
 }
 
-export default props => (
-    <AppContext.Consumer>
-        {appContext => <GroupSetsDropdown d2={appContext.d2} {...props} />}
-    </AppContext.Consumer>
-)
+const mapStateToProps = state => ({
+    collection: state.orgUnitGroupSets.collection,
+    selected: state.orgUnitGroupSets.selected,
+})
+
+const mapDispatchToProps = dispatch => ({
+    selectGroupSet: e => dispatch(setGroupSet(e.target.value)),
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(GroupSetsDropdown)
