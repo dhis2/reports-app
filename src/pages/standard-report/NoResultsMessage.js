fd64d04bef6e0238fbe373e6c94fe567e4915565
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-02-28 10:16:54 +0100

    refactor: replace keys with strings in NoResultsMessage

--- src/pages/standard-report/NoResultsMessage.js
+++ src/pages/standard-report/NoResultsMessage.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import i18n from '../../utils/i18n/locales'
-import { i18nKeys } from '../../utils/i18n/i18nKeys'
 
 const NoResultsMessage = ({ additionalStyles }) => (
     <p
         id="no-std-report-find-message-id"
         style={{ textAlign: 'center', ...additionalStyles }}
     >
-        {i18n.t(i18nKeys.messages.noResultsFound)}
+        {i18n.t('No results have been found')}
     </p>
 )
 
