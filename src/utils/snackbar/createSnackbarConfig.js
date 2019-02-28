09ec36db2d1657c9c9ecd016f276bf40a1b457bb
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-02-28 08:50:03 +0100

    refactor: replace keys with strings in snackbar config

--- src/utils/snackbar/createSnackbarConfig.js
+++ src/utils/snackbar/createSnackbarConfig.js
@@ -1,13 +1,13 @@
 import i18n from '../../utils/i18n/locales'
-import { i18nKeys } from '../../utils/i18n/i18nKeys'
 import { ACTION_MESSAGE } from '../../utils/feedbackTypes.js'
 
+const action = i18n.t('Confirm delete')
 const createSnackbarConfig = state => {
     if (state.standardReport.requestDelete) {
         return {
             type: ACTION_MESSAGE,
             message: state.standardReport.selectedReport.displayName,
-            action: i18n.t(i18nKeys.messages.confirmDelete),
+            action,
         }
     }
 
