74ea1825155ba50658ffa90049e444bf23480486
Hendrik de Graaf <hendrik@dhis2.org>
2019-03-07 11:05:29 +0100

    fix(bar-chart): prepare state and actions for rendering chart

--- src/redux/reducers/orgUnitDistReport.js
+++ src/redux/reducers/orgUnitDistReport.js
@@ -1,38 +1,16 @@
 import { actionTypes } from '../actions/orgUnitDistReport'
-import { actionTypes as reportDataActionTypes } from '../actions/reportData'
 
 export const defaultState = {
-    displayImage: false,
-    chartImagelLoading: false,
+    shouldShowChart: false,
 }
 
 export const orgUnitDistReport = (state = defaultState, { type } = {}) => {
     switch (type) {
-        case actionTypes.CHART_IMAGE_URL_LOADING_START:
-            return {
-                ...state,
-                displayImage: true,
-                chartImageLoading: true,
-            }
+        case actionTypes.SET_TABULAR_OUTPUT:
+            return { shouldShowChart: false }
 
-        case actionTypes.CHART_IMAGE_URL_LOADING_SUCCESS:
-            return {
-                ...state,
-                chartImageLoading: false,
-            }
-
-        case actionTypes.CHART_IMAGE_URL_LOADING_ERROR:
-            return {
-                ...state,
-                displayImage: false,
-                chartImageLoading: false,
-            }
-
-        case reportDataActionTypes.LOADING_REPORT_DATA_START:
-            return {
-                ...state,
-                displayImage: false,
-            }
+        case actionTypes.SET_CHART_OUTPUT:
+            return { shouldShowChart: true }
 
         default:
             return state
