13f84c4ca392f8190a46fc093f4622da4fe251bf
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-03-20 16:18:02 +0100

    fix: hide edit action for non-external resources due to backend bug

--- src/pages/resource/helper.js
+++ src/pages/resource/helper.js
@@ -7,9 +7,14 @@ export const showContextAction = deleteResource => (document, action) => {
     const access = document && document.access ? document.access : {}
     const actions = {
         [resourceActions.VIEW]: access.read,
-        [resourceActions.EDIT]: access.update,
         [resourceActions.SHARING_SETTINGS]: access.manage || access.externalize,
         [resourceActions.DELETE]: deleteResource,
+
+        /**
+         * This is currently not working on the backend for non-external documents
+         * See the docs (docs/sections/resources.md) in this repo for more details
+         */
+        [resourceActions.EDIT]: access.update && document.external,
     }
 
     return actions[action] || false
