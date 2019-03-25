e93b224aa3cb92ca72e2c64f4eab35907ba95301
Hendrik de Graaf <hendrik@dhis2.org>
2019-03-25 15:24:57 +0100

    fix(jest): wip trying to mock styled-jsx css module

--- src/__mocks__/styled-jsx/css.js
+++ src/__mocks__/styled-jsx/css.js
@@ -1,5 +1,11 @@
+const returnEmpyString = () => ''
 function css() {
-    return ''
+    return returnEmpyString()
 }
 
+css.resolve = returnEmpyString
+css.global = returnEmpyString
+
 module.exports = css
+module.exports.global = css.global
+module.exports.resolve = css.resolve
--- src/components/form/CheckBox.js
+++ src/components/form/CheckBox.js
@@ -1,4 +1,4 @@
-import { Checkbox } from '@material-ui/core'
+import Checkbox from '@material-ui/core/Checkbox'
 import css from 'styled-jsx/css'
 import PropTypes from 'prop-types'
 import React from 'react'
