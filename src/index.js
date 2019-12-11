a290ae1ca67cfe6cadce4f48dd747f2d9f191a81
Viktor Varland <varl@vlv.io>
2019-12-11 14:23:10 +0100

    chore: upgrade headerbar to the latest and greatest

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 import '@babel/polyfill'
-import { ConnectedRouter } from 'connected-react-router'
-import { getManifest, getUserSettings, init } from 'd2'
+import 'typeface-roboto'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import { Provider } from 'react-redux'
-import 'typeface-roboto'
-import App from './App'
+import { ConnectedRouter } from 'connected-react-router'
+
+import { getManifest, getUserSettings, init } from 'd2'
+import { Provider as RuntimeProvider } from '@dhis2/app-runtime'
+import { CssReset } from '@dhis2/ui-core'
+
 import store from './redux/store'
-import registerServiceWorker from './registerServiceWorker'
 import { getD2, initApi } from './utils/api'
 import history from './utils/history'
 import { configI18n } from './utils/i18n/configI18n'
 import injectTranslationsToD2 from './utils/i18n/injectTranslationsToD2'
 
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
 getManifest('manifest.webapp').then(manifest => {
     const baseUrl =
         process.env.NODE_ENV === 'production'
-            ? `${manifest.getBaseUrl()}/api/${manifest.dhis2.apiVersion}`
-            : `${process.env.REACT_APP_DHIS2_BASE_URL}/api/${
-                  manifest.dhis2.apiVersion
-              }`
+            ? manifest.getBaseUrl()
+            : process.env.REACT_APP_DHIS2_BASE_URL
+
+    const apiVersion = manifest.dhis2.apiVersion
+    const apiUrl = `${baseUrl}/api/${apiVersion}`
 
     // init d2 with configs
     init({
-        baseUrl,
+        baseUrl: apiUrl,
         schemas: [
             'dataApprovalLevel',
             'dataSet',
@@ -41,11 +49,19 @@ getManifest('manifest.webapp').then(manifest => {
             const d2Instance = getD2()
             injectTranslationsToD2(d2Instance)
             ReactDOM.render(
-                <Provider store={store}>
-                    <ConnectedRouter history={history}>
-                        <App d2={d2Instance} />
-                    </ConnectedRouter>
-                </Provider>,
+                <RuntimeProvider
+                    config={{
+                        baseUrl,
+                        apiVersion,
+                    }}
+                >
+                    <Provider store={store}>
+                        <ConnectedRouter history={history}>
+                            <CssReset />
+                            <App d2={d2Instance} />
+                        </ConnectedRouter>
+                    </Provider>
+                </RuntimeProvider>,
                 document.getElementById('app')
             )
         })
