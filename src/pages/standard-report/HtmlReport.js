78928a5846f6fdccfb944214433c609c977ccfcb
Hendrik de Graaf <hendrik@dhis2.org>
2020-08-20 09:14:18 +0200

    feat: introduce iframe auto-height according to content functionality

--- src/pages/standard-report/HtmlReport.js
+++ src/pages/standard-report/HtmlReport.js
@@ -1,11 +1,12 @@
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import React from 'react'
 import { getContextPath } from '../../utils/api'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 const SCRIPTS = [
     '/dhis-web-commons/javascripts/jQuery/jquery.min.js',
     '/dhis-web-commons/javascripts/dhis2/dhis2.util.js',
+    '/dhis-web-commons/oust/oust.js',
 ]
 
 const wrapHtmlInTemplate = html => `
@@ -37,26 +38,67 @@ const Loader = () => (
     </div>
 )
 
-const HtmlReport = ({ html }) =>
-    html ? (
-        <iframe
-            id="html-report-id"
-            srcDoc={wrapHtmlInTemplate(html)}
-            title="html-report-content"
-            width="100%"
-            seamless={true}
-            sandbox="allow-same-origin allow-scripts allow-modals"
-        >
-            <style jsx>{`
-                iframe {
-                    border: none;
-                    flex-grow: 1;
-                }
-            `}</style>
-        </iframe>
-    ) : (
-        <Loader />
-    )
+class HtmlReport extends Component {
+    state = { heigth: 'auto' }
+    iframeHtml = null
+    iframeBody = null
+    heightObserver = null
+
+    onIframeLoad = event => {
+        const iframe = event.target
+
+        this.iframeHtml = iframe.contentWindow.document.documentElement
+        this.iframeBody = iframe.contentWindow.document.body
+        this.heightObserver = new window.ResizeObserver(this.adjustHeight)
+
+        this.heightObserver.observe(this.iframeHtml)
+        this.heightObserver.observe(this.iframeBody)
+        this.adjustHeight()
+    }
+
+    adjustHeight = () => {
+        const height = Math.max(
+            this.iframeBody.clientHeight,
+            this.iframeBody.scrollHeight,
+            this.iframeBody.offsetHeight,
+            this.iframeHtml.clientHeight,
+            this.iframeHtml.scrollHeight,
+            this.iframeHtml.offsetHeight
+        )
+
+        // Add 20px as a "safety margin" in case we get a horizontal scroll bar
+        this.setState({ height: height + 20 })
+    }
+
+    componentWillUnmount() {
+        this.heightObserver && this.heightObserver.unobserve(this.iframeHtml)
+        this.heightObserver && this.heightObserver.unobserve(this.iframeBody)
+    }
+
+    render() {
+        return this.props.html ? (
+            <iframe
+                id="html-report-id"
+                srcDoc={wrapHtmlInTemplate(this.props.html)}
+                title="html-report-content"
+                width="100%"
+                height={this.state.height}
+                seamless={true}
+                sandbox="allow-same-origin allow-scripts allow-modals"
+                onLoad={this.onIframeLoad}
+            >
+                <style jsx>{`
+                    iframe {
+                        border: none;
+                        flex-grow: 1;
+                    }
+                `}</style>
+            </iframe>
+        ) : (
+            <Loader />
+        )
+    }
+}
 
 HtmlReport.propTypes = {
     html: PropTypes.string.isRequired,
