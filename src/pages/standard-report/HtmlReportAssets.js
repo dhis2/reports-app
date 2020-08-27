f6e3f095c2c8c39bc0e448ab64c182db53ba229b
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2020-08-27 13:37:51 +0200

    feat: add all assets from the old struts app to the html report iframe (#142)
    * feat: add all assets from the old struts app to the html report iframe
    
    * fix(html-report): limit number of assets after checking with Lars

---
+++ src/pages/standard-report/HtmlReportAssets.js
@@ -0,0 +1,28 @@
+export const CSS_FILES = [
+    {
+        media: 'screen',
+        styleSheet:
+            '/dhis-web-commons/javascripts/jQuery/ui/css/redmond/jquery-ui.css',
+    },
+    {
+        media: 'print',
+        styleSheet: '/dhis-web-commons/css/print.css',
+    },
+    {
+        media: 'screen,print',
+        styleSheet: '/dhis-web-commons/css/themes/light_blue/light_blue.css',
+    },
+]
+
+export const SCRIPT_FILES = [
+    '/dhis-web-commons/javascripts/jQuery/jquery.min.js',
+    '/dhis-web-commons/javascripts/jQuery/jquery.utils.js',
+    '/dhis-web-commons/javascripts/jQuery/ui/jquery-ui.min.js',
+    '/dhis-web-commons/javascripts/dhis2/dhis2.util.js',
+    '/dhis-web-commons/javascripts/commons.js',
+    '/dhis-web-commons/javascripts/commons.ajax.js',
+    '/dhis-web-commons/javascripts/lists.js',
+    '/dhis-web-commons/javascripts/periodType.js',
+    '/dhis-web-commons/javascripts/date.js',
+    '/dhis-web-commons/oust/oust.js',
+]
