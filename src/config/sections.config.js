8dcaf24e93ed33958ba8aa5c9f8df402ab6e8a22
Hendrik de Graaf <hendrik@dhis2.org>
2020-09-01 11:57:53 +0200

    chore: fix some untranslated strings

--- src/config/sections.config.js
+++ src/config/sections.config.js
@@ -1,4 +1,4 @@
-import i18n from '@dhis2/d2-i18n'
+import i18n from '../locales'
 
 export const STANDARD_REPORT_SECTION_KEY = 'standard-report'
 export const DATA_SET_REPORT_SECTION_KEY = 'data-set-report'
