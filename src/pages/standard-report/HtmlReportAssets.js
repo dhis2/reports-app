1f20bf8f061d2697900621f1e9f5b13776460e94
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2020-09-08 14:50:23 +0200

    fix: add dhis2 period utility (#164)

--- src/pages/standard-report/HtmlReportAssets.js
+++ src/pages/standard-report/HtmlReportAssets.js
@@ -19,6 +19,7 @@ export const SCRIPT_FILES = [
     '/dhis-web-commons/javascripts/jQuery/jquery.utils.js',
     '/dhis-web-commons/javascripts/jQuery/ui/jquery-ui.min.js',
     '/dhis-web-commons/javascripts/dhis2/dhis2.util.js',
+    '/dhis-web-commons/javascripts/dhis2/dhis2.period.js',
     '/dhis-web-commons/javascripts/commons.js',
     '/dhis-web-commons/javascripts/commons.ajax.js',
     '/dhis-web-commons/javascripts/lists.js',
