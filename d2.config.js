e146e13913aa2f73216680892bfcfc09fe00a506
Viktor Varland <viktor@dhis2.org>
2021-11-18 13:38:44 +0100

    chore(release): migrate to new app version scheme
    Align all our application versions that range between 0.x.x and 36.x.x
    with different semantics to a single version scheme with distinct
    semantics for what constitutes a breaking change.
    
    We have chosen to bump to v100.0.0 to signify the depature from the old
    version scheme to the new.
    
    For more information:
    dhis2/notes#293
    
    BREAKING CHANGE: App version becomes decoupled from DHIS2 versions, see
    the d2.config.js or App Hub for DHIS2 version compatibility.

--- d2.config.js
+++ d2.config.js
@@ -2,6 +2,8 @@ const config = {
     type: 'app',
     coreApp: true,
     title: 'Reports',
+    id: 'b7e619e4-5b9f-4b8b-a4ff-9874f197ae39',
+    minDHIS2Version: '2.33',
     name: 'reports',
     entryPoints: {
         app: './src/AppWrapper.js',
