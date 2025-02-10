e2631dc1b6aa08148b59aab9e4f0dc5de24fa57a
Kai Vandivier <kpvandivier@gmail.com>
2025-02-10 20:41:08 +0100

    test: polyfill TextEncoder; update snapshot

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
+import util from 'util'
 import Adapter from '@cfaester/enzyme-adapter-react-18'
 import { configure } from 'enzyme'
+
+Object.defineProperty(global, 'TextEncoder', {
+    value: util.TextEncoder,
+})
 
 configure({ adapter: new Adapter() })
